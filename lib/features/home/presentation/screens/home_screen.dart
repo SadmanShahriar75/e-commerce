4168b8e5a150c8c424eb381c1a40da8594364ef2
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2026-03-24 06:29:08 +0600

    home ui half done

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:crafy_bay/app/asset_paths.dart';
+import 'package:crafy_bay/features/home/presentation/widgets/circle_icon_button.dart';
+import 'package:crafy_bay/features/home/presentation/widgets/home_carousel_slider.dart';
+import 'package:crafy_bay/features/home/presentation/widgets/product_search_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,10 +11,33 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Center(child: Text("Home", style: TextStyle(fontSize: 33)))],
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            spacing: 16,
+            children: [ProductSearchField(), HomeCarouselSlider()],
+          ),
+        ),
       ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: SvgPicture.asset(AssetPaths.navLogoSvg),
+      actions: [
+        CircleIconButton(onTap: () {}, icon: Icons.person),
+        const SizedBox(width: 4),
+        CircleIconButton(onTap: () {}, icon: Icons.call),
+        const SizedBox(width: 4),
+        CircleIconButton(
+          onTap: () {},
+          icon: Icons.notifications_active_outlined,
+        ),
+        const SizedBox(width: 4),
+      ],
     );
   }
 }
---
+++ lib/features/home/presentation/widgets/home_carousel_slider.dart
@@ -0,0 +1,73 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:crafy_bay/app/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeCarouselSlider extends StatefulWidget {
+  const HomeCarouselSlider({super.key});
+
+  @override
+  State<HomeCarouselSlider> createState() => _HomeCarouselSliderState();
+}
+
+class _HomeCarouselSliderState extends State<HomeCarouselSlider> {
+  final ValueNotifier<int> _seletedIndex = ValueNotifier(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 200,
+            viewportFraction: 1,
+            onPageChanged: (index, reason) {
+              _seletedIndex.value = index;
+            },
+            autoPlay: true,
+          ),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text('text $i', style: TextStyle(fontSize: 16.0)),
+                );
+              },
+            );
+          }).toList(),
+        ),
+        const SizedBox(height: 8),
+
+        ValueListenableBuilder(
+          valueListenable: _seletedIndex,
+          builder: (context, selectedIndex, _) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (int i = 0; i < 5; i++)
+                  Container(
+                    width: 12,
+                    height: 12,
+                    margin: EdgeInsets.only(right: 4),
+                    decoration: BoxDecoration(
+                      color: i == _seletedIndex.value
+                          ? AppColors.themeColor
+                          : null,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+              ],
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
